6d608571c6c3f9ab7ea780e5f88e2cf15a47b3e4
MethHeadAlert <RycheIsGod@live.com>
2014-04-17 21:19:56 -0700

    Update m7-common.mk

--- m7-common.mk
+++ m7-common.mk
@@ -24,8 +24,8 @@ DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
 TARGET_SCREEN_HEIGHT := 1920
 TARGET_SCREEN_WIDTH := 1080
 
-PRODUCT_PACKAGES += \
-    Launcher3
+PRODUCT_COPY_FILES += \
+    device/htc/m7-common/rootdir/apps/com.teslacoilsw.launcher-1.apk:data/app/com.teslacoilsw.launcher-1.apk
 
 # Ramdisk
 PRODUCT_PACKAGES += \
